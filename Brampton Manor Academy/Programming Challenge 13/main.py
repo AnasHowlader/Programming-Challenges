6e20bfe71788d26b10b7127ba0cf2e126677f0c5
AnasHowlader <112864700+AnasHowlader@users.noreply.github.com>
2022-09-28 09:58:32 +0100

    Add files via upload

--- Brampton Manor Academy/Programming Challenge 13/main.py
+++ Brampton Manor Academy/Programming Challenge 13/main.py
@@ -11,50 +11,50 @@ def scrape(url):
     return mystr
 
 
-def pos_finder(html):
-    k= 0
-    while k != 10:
-        open_tr = html.find('<tr>', k)
-        close_tr: object = html.find("</tr>", open_tr)
+def pos_finder(newstring):
+    lookingfor = '"position">'
+    pos_start = newstring.find(lookingfor)
+    pos_end = newstring.find('</', pos_start)
+    position = (newstring[pos_start + len(lookingfor):pos_end])
 
-        newstring = html[open_tr:close_tr]
-        lookingfor = '"position">'
-        pos_start = newstring.find(lookingfor)
-        pos_end = newstring.find('</', pos_start)
-        position = (newstring[pos_start + len(lookingfor):pos_end])
-
-        return position
-        k += 1
+    return position
 
 
 
-def name_finder(html):
-    v = 0
-    open_tr_2 = html.find('<tr>', v)
-    close_tr_2 : object = html.find("</tr>", open_tr_2)
-
-    namestring = html[open_tr_2:close_tr_2]
-    lookingforname = '<a'
-    name_start = namestring.find(lookingforname)
-    name_end = namestring.find('</a>', name_start)
-    name = (namestring[name_start + 47:name_end])
+def name_finder(newstring):
+    lookingforname = newstring.find('<a href')
+    name_start = newstring.find('/">', lookingforname)
+    name_end = newstring.find('</a>', name_start)
+    name = (newstring[name_start + 3:name_end])
     return name
 
-def artist_finder(html):
-    s = 0
-    open_tr_3 = html.find('class="artist"', s)
-    close_tr_3: object = html.find("</tr>", open_tr_3)
-
-    artiststring = html[open_tr_3:close_tr_3]
-    lookingforartist = '<a'
-    artist_start = artiststring.find(lookingforartist)
-    artist_end = artiststring.find('</a>', artist_start)
-    artist = (artiststring[artist_start + 52:artist_end])
+def artist_finder(newstring):
+    lookingforartist = newstring.find('<div class="artist">')
+    artist_start = newstring.find('/">', lookingforartist)
+    artist_end = newstring.find('</a>', artist_start)
+    artist = (newstring[artist_start + 3:artist_end])
     return artist
+
+def table():
+    i = 0
+    num = 0
+    while num != 10:
+        open_tr = html.find('<tr>', i)
+        close_tr = html.find("</tr>", open_tr)
+
+        newstring = html[open_tr:close_tr]
+
+
+        position = pos_finder(newstring)
+        name = name_finder(newstring)
+        artist = artist_finder(newstring)
+
+        print(f'{position}  {name}  {artist}')
+
+        i = close_tr
+        num +=1
 
 if __name__ == "__main__":
     html = scrape(url)
-    print(pos_finder(html), end =' ')
-    print(name_finder(html), end =' ')
-    print(artist_finder(html),)
+    table()
 
