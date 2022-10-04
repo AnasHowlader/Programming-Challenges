7e320d26eacb32b17135d681a8434abed06bae4e
AnasHowlader <112864700+AnasHowlader@users.noreply.github.com>
2022-10-04 10:45:44 +0100

    Add files via upload

--- Brampton Manor Academy/Programming Challenge 15/main.py
+++ Brampton Manor Academy/Programming Challenge 15/main.py
@@ -2,53 +2,76 @@ import csv
 
 from pathlib import Path
 
-csv_file = Path("south.csv")
-html_file = Path("south.html.pdf")
-
-def check_file_exists(path):
-    return csv_file.is_file()
 
 def read_csv(path):
-    csv_contents = []
-    if check_file_exists(csv_file):
-        with open(csv_file) as csvfile:
-            reader = csv.reader(csvfile, delimiter=",")
-            next(reader)
-            for row in reader:
-                csv_contents.append(row)
+    with open(path) as csvfile:
+        csv_contents = []
+        reader = csv.reader(csvfile, delimiter=",")
+        for row in reader:
+            csv_contents.append(row)
     return csv_contents
 
 
-file_contents = read_csv(csv_file)
-
-
 def read_html(path):
-    html_contents = []
-    html_contents = open(html_file, "r")
+    html_file = open(path, "r")
+    html_contents = html_file.read()
     return html_contents
 
-html_contents = read_html(html_file)
+
 
 def process(csv, html):
+    i = 0
+    v = 0
+    s = 0
+    k= 0
+    counter = 0
+    while counter != 5:
+        while k < 5:
 
-    lookingfor = 'url("'
-    url_start = html.find(lookingfor)
-    url_end = html.find('");', url_start)
-    url = html[url_start:url_end]
-    url.append(csv())
+
+            lookingfor = 'url("'
+            url_start = html.find(lookingfor, i)
+            url_end = html.find('");', url_start)
+            dummy_link = html[url_start:url_end]
+            print(dummy_link)
+            k+=1
+
+
+
+
+            initials_start = html.find('"el__heading">', v)
+            initials_end = html.find("<", initials_start)
+
+
+
+
+            name_start = html.find('el__text">', s)
+            name_end = html.find("<", name_start)
+
+
+
+            i = url_end
+
+            v = initials_end
+
+            s = name_end
+            counter += 1
 
 
 
 def write_html(path, html):
-    html = open(html_file, "w")
-    html.write(process(path))
+    html_write = open(path, "w")
+    html_write.write(html)
+    html_write.close()
 
 
 
 
-
-print(file_contents)
-
+if __name__ == "__main__":
+    csv = read_csv(path="south.csv")
+    html = read_html(path="south.html")
+    html = process(csv=csv, html=html)
+    write_html(path="south_final.html", html=html)
 
 
 
