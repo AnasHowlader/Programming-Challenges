fbbda7b3b89293903f222b49b25e19d8204fd8d2
AnasHowlader <112864700+AnasHowlader@users.noreply.github.com>
2022-10-07 08:09:11 +0100

    Add files via upload

--- Brampton Manor Academy/Programming Challenge 15/main.py
+++ Brampton Manor Academy/Programming Challenge 15/main.py
@@ -1,7 +1,5 @@
 import csv
 
-from pathlib import Path
-
 
 def read_csv(path):
     with open(path) as csvfile:
@@ -21,47 +19,47 @@ def read_html(path):
 
 def process(csv, html):
     i = 0
-    v = 0
-    s = 0
+    v= 0
+    s= 0
     k= 0
     counter = 0
     while counter != 5:
-        while k < 5:
-
+        for k in range (0,5):
 
             lookingfor = 'url("'
             url_start = html.find(lookingfor, i)
             url_end = html.find('");', url_start)
-            dummy_link = html[url_start:url_end]
-            print(dummy_link)
-            k+=1
+            newlink = html.replace(html[url_start + len(lookingfor):url_end], csv[0+k][0])
+            print(html)
+
 
 
 
 
             initials_start = html.find('"el__heading">', v)
             initials_end = html.find("<", initials_start)
+            initials = csv[0][0+k]
+            replacelink = html.replace(html[initials_start:initials_end], initials)
+
 
 
 
 
             name_start = html.find('el__text">', s)
             name_end = html.find("<", name_start)
-
-
-
             i = url_end
-
-            v = initials_end
-
+            v= initials_end
             s = name_end
             counter += 1
+            k+=1
+            return newlink
+
 
 
 
 def write_html(path, html):
     html_write = open(path, "w")
-    html_write.write(html)
+    html_write.write(str(html))
     html_write.close()
 
 
