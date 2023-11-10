23fd5b8fcb03064c773ff9fae1e461d020ca2e7d
AnasHowlader <112864700+AnasHowlader@users.noreply.github.com>
2023-11-10 09:31:49 +0000

    Add files via upload

---
+++ prerelease 2024/rotations.py
@@ -0,0 +1,22 @@
+class Pattern():
+    def __init__(self, SymbolToUse, PatternString):
+        self.__Symbol = SymbolToUse
+        self.__PatternSequence = PatternString
+
+    def MatchesPattern(self, PatternString, SymbolPlaced):
+        if SymbolPlaced != self.__Symbol:
+            return False
+        for Count in range(0, len(self.__PatternSequence)):
+            try:
+                if self.__PatternSequence[Count] == self.__Symbol and PatternString[Count] != self.__Symbol:
+                    return False
+            except Exception as ex:
+                print(f"EXCEPTION in MatchesPattern: {ex}")
+        return True
+
+    def MatchesRotation(self, PatternString, SymbolPlaced):
+        Pattern = self.__PatternSequence
+
+
+    def GetPatternSequence(self):
+      return self.__PatternSequence
